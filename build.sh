32f37e85baabde978123e0262e4b0a4ae149500a
sunshanqian <sunshanqian@outlook.com>
2023-04-14 15:53:06 +0900

    202304141552

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ rm musicianAPP
 rm /home/ssq/workdir/nfs/*.ms
 arm-linux-gnueabihf-gcc main.c cJSON.c -o musicianAPP
 cp musicianAPP /home/ssq/workdir/nfs
-cp *.ms /home/ssq/workdir/nfs
+cp ms/*.ms /home/ssq/workdir/nfs
+cp testms/*.ms /home/ssq/workdir/nfs
